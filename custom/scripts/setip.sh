378adbc5607ff510ebf80b17c8117111f09edd4f
selfcan <99958756+selfcan@users.noreply.github.com>
2025-01-26 12:38:54 +0800

    Update setip.sh

--- custom/scripts/setip.sh
+++ custom/scripts/setip.sh
@@ -9,7 +9,7 @@ x=${x#*.*}
 oc3=${x%%.*}
 dhcpfrom=$oc1"."$oc2"."$oc3".100"
 dhcpto=$oc1"."$oc2"."$oc3".244"
-sed -i "s/\"192.168.2.1\"/\"$lanip\"/g" $defaultfile
-sed -i "s/\"192.168.2.100\"/\"$dhcpfrom\"/g" $defaultfile
-sed -i "s/\"192.168.2.244\"/\"$dhcpto\"/g" $defaultfile
-sed -i "s/192.168.2.1/$lanip/" $dicfile
+sed -i "s/\"10.10.10.10\"/\"$lanip\"/g" $defaultfile
+sed -i "s/\"10.10.10.100\"/\"$dhcpfrom\"/g" $defaultfile
+sed -i "s/\"10.10.10.244\"/\"$dhcpto\"/g" $defaultfile
+sed -i "s/10.10.10.10/$lanip/" $dicfile
